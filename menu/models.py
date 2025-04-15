cf50f340425e7004a027814cc1c091b5880f0077
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-15 16:01:39 +0200

    Adding accounts app

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -16,3 +17,11 @@ class Dish(models.Model):
         Category, on_delete=models.CASCADE, related_name="dishes"
     )
     is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
