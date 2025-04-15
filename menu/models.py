1dfe9c2afa0e704cdc608e77cc6cdd49116c8f62
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-15 16:37:58 +0200

    Add signup logic

--- menu/models.py
+++ menu/models.py
@@ -25,3 +25,6 @@ class Dish(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f""
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/menu/", include("menu.urls")),
+    path("restaurant/accounts/", include("accounts.urls")),
 ]
